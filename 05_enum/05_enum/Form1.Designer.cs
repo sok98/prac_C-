2d23bfa513318cf524889b560a3d8486f8d66cdb
Seo Yeseul <sok98@naver.com>
2021-11-05 10:14:17 +0900

    05_enum

---
+++ 05_enum/05_enum/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace _05_enum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxName = new System.Windows.Forms.TextBox();
+            this.lboxDay = new System.Windows.Forms.ListBox();
+            this.lboxTime = new System.Windows.Forms.ListBox();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tboxName
+            // 
+            this.tboxName.Location = new System.Drawing.Point(12, 12);
+            this.tboxName.Name = "tboxName";
+            this.tboxName.Size = new System.Drawing.Size(100, 25);
+            this.tboxName.TabIndex = 0;
+            // 
+            // lboxDay
+            // 
+            this.lboxDay.FormattingEnabled = true;
+            this.lboxDay.ItemHeight = 15;
+            this.lboxDay.Location = new System.Drawing.Point(12, 43);
+            this.lboxDay.Name = "lboxDay";
+            this.lboxDay.Size = new System.Drawing.Size(212, 214);
+            this.lboxDay.TabIndex = 1;
+            // 
+            // lboxTime
+            // 
+            this.lboxTime.FormattingEnabled = true;
+            this.lboxTime.ItemHeight = 15;
+            this.lboxTime.Location = new System.Drawing.Point(230, 43);
+            this.lboxTime.Name = "lboxTime";
+            this.lboxTime.Size = new System.Drawing.Size(212, 214);
+            this.lboxTime.TabIndex = 2;
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(12, 280);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 3;
+            this.btnResult.Text = "결과보기";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "StringFormat Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Enabled = false;
+            this.tboxResult.Location = new System.Drawing.Point(12, 309);
+            this.tboxResult.Multiline = true;
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(430, 56);
+            this.tboxResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 375);
+            this.Controls.Add(this.tboxResult);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.lboxTime);
+            this.Controls.Add(this.lboxDay);
+            this.Controls.Add(this.tboxName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxName;
+        private System.Windows.Forms.ListBox lboxDay;
+        private System.Windows.Forms.ListBox lboxTime;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tboxResult;
+    }
+}
+
---
+++ 05_enum/05_enum/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_enum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum enumDay
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thusday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        enum enumTime
+        {
+            Morning,
+            Afternoon,
+            Evening
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lboxDay.Items.Add(enumDay.Monday);
+            lboxDay.Items.Add(enumDay.Tuesday);
+            lboxDay.Items.Add(enumDay.Wednesday);
+            lboxDay.Items.Add(enumDay.Thusday);
+            lboxDay.Items.Add(enumDay.Friday);
+            lboxDay.Items.Add(enumDay.Saturday);
+            lboxDay.Items.Add(enumDay.Sunday);
+
+            lboxTime.Items.Add(enumTime.Morning);
+            lboxTime.Items.Add(enumTime.Afternoon);
+            lboxTime.Items.Add(enumTime.Evening);
+        }
+
+        // 00와 0요일 00에 보기로 했습니다.
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            tboxResult.Text = tboxName.Text + "와 " + lboxDay.SelectedItem + "(요일) " 
+                + lboxTime.SelectedItem + "에 보기로 했습니다.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tboxResult.Text = string.Format("{0}와 {1}(요일) {2}에 보기로 했습니다.",
+                tboxName.Text, lboxDay.SelectedItem, lboxTime.SelectedItem);
+        }
+    }
+}
