9507ae4fd70e14cf0dc52734e0b4ca98565aa16f
Seo Yeseul <sok98@naver.com>
2021-11-05 10:27:54 +0900

    06_summary

--- 05_enum/05_enum/Form1.cs
+++ 05_enum/05_enum/Form1.cs
@@ -59,8 +59,22 @@ namespace _05_enum
 
         private void button2_Click(object sender, EventArgs e)
         {
-            tboxResult.Text = string.Format("{0}와 {1}(요일) {2}에 보기로 했습니다.",
-                tboxName.Text, lboxDay.SelectedItem, lboxTime.SelectedItem);
+            tboxResult.Text = resultText(tboxName.Text, lboxDay.SelectedItem.ToString(), lboxTime.SelectedItem.ToString());
+        }
+
+        /// <summary>
+        /// 함수에 대한 주석을 다는 공간입니다.
+        /// ///로 표현합니다.
+        /// "00와 0(요일) 00에 보기로 했습니다."의 형태로 반환합니다.
+        /// </summary>
+        /// <param name="name">이름</param>
+        /// <param name="day">요일</param>
+        /// <param name="time">시간</param>
+        /// <returns>"00와 0(요일) 00에 보기로 했습니다."</returns>
+        private string resultText(string name, string day, string time)
+        {
+            string result = string.Format("{0}와 {1}(요일) {2}에 보기로 했습니다.", name, day, time);
+            return result;
         }
     }
 }
