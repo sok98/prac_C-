6cd82136b91657af73ed6b52f94db84876db45dd
Seo Yeseul <sok98@naver.com>
2021-11-05 11:07:36 +0900

    07_array

---
+++ 07_array/07_array/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace _07_array
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgDay = new System.Windows.Forms.DataGridView();
+            this.colDay1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.week1 = new System.Windows.Forms.Button();
+            this.week2 = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "매장 방문 수";
+            // 
+            // dgDay
+            // 
+            this.dgDay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDay.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colDay1,
+            this.colDay2,
+            this.colDay3,
+            this.colDay4,
+            this.colDay5,
+            this.colDay6,
+            this.colDay7});
+            this.dgDay.Location = new System.Drawing.Point(12, 37);
+            this.dgDay.Name = "dgDay";
+            this.dgDay.RowTemplate.Height = 27;
+            this.dgDay.Size = new System.Drawing.Size(881, 119);
+            this.dgDay.TabIndex = 1;
+            // 
+            // colDay1
+            // 
+            this.colDay1.HeaderText = "월";
+            this.colDay1.Name = "colDay1";
+            // 
+            // colDay2
+            // 
+            this.colDay2.HeaderText = "화";
+            this.colDay2.Name = "colDay2";
+            // 
+            // colDay3
+            // 
+            this.colDay3.HeaderText = "수";
+            this.colDay3.Name = "colDay3";
+            // 
+            // colDay4
+            // 
+            this.colDay4.HeaderText = "목";
+            this.colDay4.Name = "colDay4";
+            // 
+            // colDay5
+            // 
+            this.colDay5.HeaderText = "금";
+            this.colDay5.Name = "colDay5";
+            // 
+            // colDay6
+            // 
+            this.colDay6.HeaderText = "토";
+            this.colDay6.Name = "colDay6";
+            // 
+            // colDay7
+            // 
+            this.colDay7.HeaderText = "일";
+            this.colDay7.Name = "colDay7";
+            // 
+            // week1
+            // 
+            this.week1.Location = new System.Drawing.Point(12, 194);
+            this.week1.Name = "week1";
+            this.week1.Size = new System.Drawing.Size(75, 23);
+            this.week1.TabIndex = 2;
+            this.week1.Text = "일주일간";
+            this.week1.UseVisualStyleBackColor = true;
+            this.week1.Click += new System.EventHandler(this.week1_Click);
+            // 
+            // week2
+            // 
+            this.week2.Location = new System.Drawing.Point(93, 194);
+            this.week2.Name = "week2";
+            this.week2.Size = new System.Drawing.Size(75, 23);
+            this.week2.TabIndex = 3;
+            this.week2.Text = "이주일간";
+            this.week2.UseVisualStyleBackColor = true;
+            this.week2.Click += new System.EventHandler(this.week2_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(187, 202);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(115, 15);
+            this.lblTotal.TabIndex = 4;
+            this.lblTotal.Text = "전체 자료 수 : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 242);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.week2);
+            this.Controls.Add(this.week1);
+            this.Controls.Add(this.dgDay);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgDay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay7;
+        private System.Windows.Forms.Button week1;
+        private System.Windows.Forms.Button week2;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ 07_array/07_array/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ArrayTest();
+        }
+
+        private void ArrayTest()
+        {
+            int[] arrTest1 = new int[5];
+            arrTest1[0] = 5;
+
+            int[] arrTest2 = new int[] { 3, 4, 5, 6 };
+
+            int[] arrTest3 = { 1, 2, 3, 4 };
+
+            int[,] arrTest4 = new int[2, 4] { { 1, 2, 3, 4 }, { 2, 3, 4, 5 } };
+
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            int len = arr.Length;
+            Array.Clear(arr, 2, 3);             // arr의 2번째부터 2개 삭제
+            Array.Resize(ref arr, 10);          // arr의 길이 10으로 변경. ref 잊지말기
+            int search = Array.IndexOf(arr, 5); // arr의 값이 5인 인덱스 가져오기
+        }
+
+        private void week1_Click(object sender, EventArgs e)
+        {
+            dgDay.Rows.Clear();
+
+            int[] arrDay = { 3, 6, 10, 11, 23, 14, 7 };
+            dgDay["colDay1", 0].Value = arrDay[0];
+            dgDay["colDay2", 0].Value = arrDay[1];
+            dgDay["colDay3", 0].Value = arrDay[2];
+            dgDay["colDay4", 0].Value = arrDay[3];
+            dgDay["colDay5", 0].Value = arrDay[4];
+            dgDay["colDay6", 0].Value = arrDay[5];
+            dgDay["colDay7", 0].Value = arrDay[6];
+
+            lblTotal.Text = string.Format("전체 자료 수 : {0}", arrDay.Length);
+        }
+
+        private void week2_Click(object sender, EventArgs e)
+        {
+            dgDay.Rows.Clear();
+            dgDay.Rows.Add();
+
+            int[,] arrDay = { { 3, 6, 10, 11, 23, 14, 7 }, { 5, 3, 9, 11, 26, 11, 4 } };
+
+            dgDay["colDay1", 0].Value = arrDay[0, 0];
+            dgDay["colDay2", 0].Value = arrDay[0, 1];
+            dgDay["colDay3", 0].Value = arrDay[0, 2];
+            dgDay["colDay4", 0].Value = arrDay[0, 3];
+            dgDay["colDay5", 0].Value = arrDay[0, 4];
+            dgDay["colDay6", 0].Value = arrDay[0, 5];
+            dgDay["colDay7", 0].Value = arrDay[0, 6];
+
+            dgDay["colDay1", 1].Value = arrDay[1, 0];
+            dgDay["colDay2", 1].Value = arrDay[1, 1];
+            dgDay["colDay3", 1].Value = arrDay[1, 2];
+            dgDay["colDay4", 1].Value = arrDay[1, 3];
+            dgDay["colDay5", 1].Value = arrDay[1, 4];
+            dgDay["colDay6", 1].Value = arrDay[1, 5];
+            dgDay["colDay7", 1].Value = arrDay[1, 6];
+
+            lblTotal.Text = string.Format("전체 자료 수 : {0}", arrDay.Length);
+        }
+    }
+}
