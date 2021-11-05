d7d1747f708eb3ed34117481c1d77d54227e95ef
Seo Yeseul <sok98@naver.com>
2021-11-05 14:27:13 +0900

    09_for_foreach

---
+++ 09_for_foreach/09_for_foreach/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace _09_for_foreach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.btnForeach = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(12, 12);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBox.Size = new System.Drawing.Size(238, 264);
+            this.txtBox.TabIndex = 0;
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(271, 12);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(102, 23);
+            this.btnFor.TabIndex = 1;
+            this.btnFor.Text = "for문";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // btnForeach
+            // 
+            this.btnForeach.Location = new System.Drawing.Point(271, 55);
+            this.btnForeach.Name = "btnForeach";
+            this.btnForeach.Size = new System.Drawing.Size(102, 24);
+            this.btnForeach.TabIndex = 2;
+            this.btnForeach.Text = "for each문";
+            this.btnForeach.UseVisualStyleBackColor = true;
+            this.btnForeach.Click += new System.EventHandler(this.btnForeach_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 300);
+            this.Controls.Add(this.btnForeach);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.txtBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.Button btnForeach;
+    }
+}
+
---
+++ 09_for_foreach/09_for_foreach/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09_for_foreach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            txtBox.Text = string.Empty;
+            StringBuilder sb = new StringBuilder();
+
+            int result = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                result += i;
+                sb.Append(string.Format("1에서 {0}까지 더하면 {1} \r\n", i, result));
+            }
+
+            txtBox.Text = sb.ToString();
+        }
+
+        private void btnForeach_Click(object sender, EventArgs e)
+        {
+            txtBox.Text = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            string[] strArr = { "a", "b", "c", "d", "e", "f", "g" };
+
+            int i = 1;
+            foreach ( var name in strArr)
+            {
+                sb.Append(string.Format("{0} : {1}반 \r\n", name, i++));
+            }
+
+            txtBox.Text = sb.ToString();
+        }
+    }
+}
