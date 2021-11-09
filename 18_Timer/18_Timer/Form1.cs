43f6a0f38a0939872b034a09e53e0fe2fe2c7280
Seo Yeseul <sok98@naver.com>
2021-11-09 11:54:42 +0900

    19_암호화, 복호화 (미완성)

--- 18_Timer/18_Timer/Form1.cs
+++ 18_Timer/18_Timer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,15 @@ namespace _18_Timer
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, string> _dData = new Dictionary<string, string>();
+        XMLControl _xml = new XMLControl();
+        string strPath = Application.StartupPath + "\\save.xml";
+
         private double iTick = 0;
         private double iTotal = 0;
 
-        private int i1Add = 1;
-        private int i1Level = 1;
+        private int i1Add = 0;
+        private int i1Level = 0;
 
         private int i3Add = 0;
         private int i3Level = 0;
@@ -31,13 +36,38 @@ namespace _18_Timer
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            if (File.Exists(strPath))
+            {
+                // File이 있을 경우 File Loading
+                _dData = _xml.fXML_Reader(strPath);
+
+                iTick = double.Parse(_dData[XMLControl._TICK]);
+                iTotal = double.Parse(_dData[XMLControl._TOTAL]);
+                i1Level = int.Parse(_dData[XMLControl._LEVEL_1]);
+                i3Level = int.Parse(_dData[XMLControl._LEVEL_3]);
+                i50Level = int.Parse(_dData[XMLControl._LEVEL_50]);
+            }
+
             Timer oTimer = new Timer();
             oTimer.Enabled = true;
-            oTimer.Interval = 100;
+            oTimer.Interval = 1000;
             oTimer.Tick += OTimer_Tick;
 
             oTimer.Start();
 
+        }
+
+        private void Form1_FormClosed(object sender, System.Windows.Forms.FormClosedEventArgs e)
+        {
+            _dData.Clear();
+
+            _dData.Add(XMLControl._TICK, iTick.ToString());
+            _dData.Add(XMLControl._TOTAL, iTotal.ToString());
+            _dData.Add(XMLControl._LEVEL_1, i1Level.ToString());
+            _dData.Add(XMLControl._LEVEL_3, i3Level.ToString());
+            _dData.Add(XMLControl._LEVEL_50, i50Level.ToString());
+
+            _xml.fXML_Writer(strPath, _dData);
         }
 
         /// <summary>
@@ -48,13 +78,17 @@ namespace _18_Timer
         private void OTimer_Tick(object sender, EventArgs e)
         {
             iTick = i1Add + i3Add + i50Add;
-            iTotal = iTotal + iTick;
+            iTotal += iTick;
 
             lblTickPoint.Text = string.Format("Total : {0} (1: {1}, 3: {2}, 50: {3})",
                 iTick.ToString(), i1Level.ToString(), i3Level.ToString(), i50Level.ToString());
             lblTotal.Text = iTotal.ToString();
         }
-
+        /// <summary>
+        /// 버튼 클릭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAdd_Click(object sender, EventArgs e)
         {
             Button obtn = sender as Button;
@@ -64,22 +98,25 @@ namespace _18_Timer
                 case "btn1Add":
                     if (iTotal > 100)
                     {
+                        iTotal -= 100;
                         i1Level++;
-                        i1Add = 1 + i1Level;
+                        i1Add = i1Level * 1;
                     }
                     break;
                 case "btn3Add":
                     if (iTotal > 300)
                     {
+                        iTotal -= 300;
                         i3Level++;
-                        i3Add = 3 + i3Level;
+                        i3Add = i3Level * 3;
                     }
                     break;
                 case "btn50Add":
                     if (iTotal > 5000)
                     {
+                        iTotal -= 5000;
                         i50Level++;
-                        i50Add = 50 + i3Level;
+                        i50Add = i50Level * 50;
                     }
                     break;
                 default:
---
+++ 18_Timer/18_Timer/XMLControl.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _18_Timer
+
+{
+    class XMLControl
+    {
+        public static string _TICK = "TICK";
+        public static string _TOTAL = "TOTAL";
+        public static string _LEVEL_1 = "LEVEL_1";
+        public static string _LEVEL_3 = "LEVEL_3";
+        public static string _LEVEL_50 = "LEVEL_50";
+        
+        /// <summary>
+        /// 읽어오기
+        /// </summary>
+        /// <param name="strXMLPath"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> fXML_Reader(string strXMLPath)
+        {
+
+            Dictionary<string, string> DXMLConfig = new Dictionary<string, string>();
+            using(XmlReader rd = XmlReader.Create(strXMLPath))
+            {
+                while (rd.Read())
+                {
+                    if (rd.IsStartElement())
+                    {
+                        if (rd.Name.Equals("SETTING"))
+                        {
+                            string strID = rd["ID"];
+                            rd.Read();
+
+                            string strTICK = rd.ReadElementContentAsString(_TICK, "");
+                            DXMLConfig.Add(_TICK, strTICK);
+
+                            string strTOTAL = rd.ReadElementContentAsString(_TOTAL, "");
+                            DXMLConfig.Add(_TOTAL, strTOTAL);
+
+                            string strLEVEL1 = rd.ReadElementContentAsString(_LEVEL_1, "");
+                            DXMLConfig.Add(_LEVEL_1, strLEVEL1);
+
+                            string strLEVEL3 = rd.ReadElementContentAsString(_LEVEL_3, "");
+                            DXMLConfig.Add(_LEVEL_3, strLEVEL3);
+
+                            string strLEVEL50 = rd.ReadElementContentAsString(_LEVEL_50, "");
+                            DXMLConfig.Add(_LEVEL_50, strLEVEL50);
+                        }
+                    }
+                }
+            }
+
+            return DXMLConfig;
+        }
+        
+        /// <summary>
+        /// 저장하기
+        /// </summary>
+        /// <param name="strXMLPath"></param>
+        /// <param name="DXMLConfig"></param>
+        public void fXML_Writer(string strXMLPath, Dictionary<string, string> DXMLConfig)
+        {
+            using (XmlWriter wr = XmlWriter.Create(strXMLPath))
+            {
+                wr.WriteStartDocument();
+
+                //SETTING
+                wr.WriteStartElement("SETTING");
+                wr.WriteAttributeString("ID", "0001");
+
+                wr.WriteElementString(_TICK, DXMLConfig[_TICK]);
+                wr.WriteElementString(_TOTAL, DXMLConfig[_TOTAL]);
+                wr.WriteElementString(_LEVEL_1, DXMLConfig[_LEVEL_1]);
+                wr.WriteElementString(_LEVEL_3, DXMLConfig[_LEVEL_3]);
+                wr.WriteElementString(_LEVEL_50, DXMLConfig[_LEVEL_50]);
+
+                wr.WriteEndElement();
+                wr.WriteEndDocument();
+
+            }
+        }
+    }
+}
