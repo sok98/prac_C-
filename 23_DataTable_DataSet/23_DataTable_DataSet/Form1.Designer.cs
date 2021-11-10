b7cdf857134fafb021b7b9b009e5d228609521a9
Seo Yeseul <sok98@naver.com>
2021-11-10 14:55:58 +0900

    23_DataTable_DataSet

---
+++ 23_DataTable_DataSet/23_DataTable_DataSet/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace _23_DataTable_DataSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbtnM = new System.Windows.Forms.RadioButton();
+            this.rbtnW = new System.Windows.Forms.RadioButton();
+            this.tboxName = new System.Windows.Forms.TextBox();
+            this.tboxRef = new System.Windows.Forms.TextBox();
+            this.cboxClass = new System.Windows.Forms.ComboBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cboxViewClass = new System.Windows.Forms.ComboBox();
+            this.dgViewClass = new System.Windows.Forms.DataGridView();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgViewClass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.cboxClass);
+            this.groupBox1.Controls.Add(this.tboxRef);
+            this.groupBox1.Controls.Add(this.tboxName);
+            this.groupBox1.Controls.Add(this.rbtnW);
+            this.groupBox1.Controls.Add(this.rbtnM);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 231);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "등록하기";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(21, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "반 : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(21, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "이름 : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(21, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "성별 : ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(21, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "특기 : ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // rbtnM
+            // 
+            this.rbtnM.AutoSize = true;
+            this.rbtnM.Location = new System.Drawing.Point(86, 112);
+            this.rbtnM.Name = "rbtnM";
+            this.rbtnM.Size = new System.Drawing.Size(58, 19);
+            this.rbtnM.TabIndex = 4;
+            this.rbtnM.TabStop = true;
+            this.rbtnM.Text = "남자";
+            this.rbtnM.UseVisualStyleBackColor = true;
+            // 
+            // rbtnW
+            // 
+            this.rbtnW.AutoSize = true;
+            this.rbtnW.Location = new System.Drawing.Point(189, 112);
+            this.rbtnW.Name = "rbtnW";
+            this.rbtnW.Size = new System.Drawing.Size(58, 19);
+            this.rbtnW.TabIndex = 5;
+            this.rbtnW.TabStop = true;
+            this.rbtnW.Text = "여자";
+            this.rbtnW.UseVisualStyleBackColor = true;
+            // 
+            // tboxName
+            // 
+            this.tboxName.Location = new System.Drawing.Point(86, 74);
+            this.tboxName.Name = "tboxName";
+            this.tboxName.Size = new System.Drawing.Size(100, 25);
+            this.tboxName.TabIndex = 6;
+            // 
+            // tboxRef
+            // 
+            this.tboxRef.Location = new System.Drawing.Point(86, 144);
+            this.tboxRef.Name = "tboxRef";
+            this.tboxRef.Size = new System.Drawing.Size(212, 25);
+            this.tboxRef.TabIndex = 7;
+            // 
+            // cboxClass
+            // 
+            this.cboxClass.FormattingEnabled = true;
+            this.cboxClass.Items.AddRange(new object[] {
+            "1반",
+            "2반",
+            "3반"});
+            this.cboxClass.Location = new System.Drawing.Point(86, 37);
+            this.cboxClass.Name = "cboxClass";
+            this.cboxClass.Size = new System.Drawing.Size(100, 23);
+            this.cboxClass.TabIndex = 8;
+            this.cboxClass.Text = "1반";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(223, 192);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "등록";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDel);
+            this.groupBox2.Controls.Add(this.dgViewClass);
+            this.groupBox2.Controls.Add(this.cboxViewClass);
+            this.groupBox2.Location = new System.Drawing.Point(12, 263);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(531, 280);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "내용 확인";
+            // 
+            // cboxViewClass
+            // 
+            this.cboxViewClass.FormattingEnabled = true;
+            this.cboxViewClass.Items.AddRange(new object[] {
+            "1반",
+            "2반",
+            "3반"});
+            this.cboxViewClass.Location = new System.Drawing.Point(6, 24);
+            this.cboxViewClass.Name = "cboxViewClass";
+            this.cboxViewClass.Size = new System.Drawing.Size(121, 23);
+            this.cboxViewClass.TabIndex = 0;
+            this.cboxViewClass.Text = "1반";
+            this.cboxViewClass.SelectedIndexChanged += new System.EventHandler(this.cboxViewClass_SelectedIndexChanged);
+            // 
+            // dgViewClass
+            // 
+            this.dgViewClass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgViewClass.Location = new System.Drawing.Point(7, 58);
+            this.dgViewClass.Name = "dgViewClass";
+            this.dgViewClass.RowTemplate.Height = 27;
+            this.dgViewClass.Size = new System.Drawing.Size(518, 216);
+            this.dgViewClass.TabIndex = 1;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(450, 23);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "삭제";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 555);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgViewClass)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ComboBox cboxClass;
+        private System.Windows.Forms.TextBox tboxRef;
+        private System.Windows.Forms.TextBox tboxName;
+        private System.Windows.Forms.RadioButton rbtnW;
+        private System.Windows.Forms.RadioButton rbtnM;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.DataGridView dgViewClass;
+        private System.Windows.Forms.ComboBox cboxViewClass;
+    }
+}
+
---
+++ 23_DataTable_DataSet/23_DataTable_DataSet/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_DataTable_DataSet
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet(); // 학급들의 정보
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool bCheckisTable = false;
+            if (ds.Tables.Contains(cboxClass.Text))
+            {
+                bCheckisTable = true;
+            }
+
+            DataTable dt = null;
+            if (!bCheckisTable)
+            {
+                dt = new DataTable(cboxClass.Text);
+
+                DataColumn colName = new DataColumn("NAME", typeof(string));
+                DataColumn colSex = new DataColumn("SEX", typeof(string));
+                DataColumn colRef = new DataColumn("REF", typeof(string));
+
+                dt.Columns.Add(colName);
+                dt.Columns.Add(colSex);
+                dt.Columns.Add(colRef);
+            }
+            else
+            {
+                dt = ds.Tables[cboxClass.Text];
+            }
+
+            // Row 자료 등록
+            DataRow row = dt.NewRow();
+
+            row["NAME"] = tboxName.Text;
+
+            if (rbtnM.Checked)
+                row["SEX"] = "남자";
+            else
+                row["SEX"] = "여자";
+
+            row["REF"] = tboxRef.Text;
+
+            // 생성된 Row -> DataTable -> DataSet 에 등록
+            if (bCheckisTable)
+            {
+                ds.Tables[cboxClass.Text].Rows.Add(row);
+            }
+            else
+            {
+                dt.Rows.Add(row);
+                ds.Tables.Add(dt);
+            }
+
+            ViewRefresh();
+        }
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (dgViewClass.SelectedRows.Count != 0)
+            {
+                int iSelectRow = dgViewClass.SelectedRows[0].Index;
+                ds.Tables[cboxViewClass.Text].Rows.RemoveAt(iSelectRow);
+                ViewRefresh();
+            }
+        }
+        private void cboxViewClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ViewRefresh();
+        }
+
+
+        private void ViewRefresh()
+        {
+            dgViewClass.DataSource = ds.Tables[cboxViewClass.Text];
+
+            // DataGridView Cell 정렬 및 Number 적용
+            foreach (DataGridViewRow row in dgViewClass.Rows)
+            {
+                row.HeaderCell.Value = row.Index.ToString();
+            }
+            dgViewClass.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+        }
+    }
+}
