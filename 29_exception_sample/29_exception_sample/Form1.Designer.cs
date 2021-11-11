6e8e433221896b86bbd1d1cfb3599a263c8da59e
Seo Yeseul <sok98@naver.com>
2021-11-11 12:23:00 +0900

    29_exception_sample_UI

---
+++ 29_exception_sample/29_exception_sample/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace _29_exception_sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbarAlpha = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblColorInfo = new System.Windows.Forms.Label();
+            this.pColor = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lboxColor = new System.Windows.Forms.ListBox();
+            this.pBackground = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbarAlpha)).BeginInit();
+            this.pBackground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 126);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.55072F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.44927F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbarAlpha, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblColorInfo, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pColor, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 21);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(276, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(0, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 33);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Alpha : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Color : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tbarAlpha
+            // 
+            this.tbarAlpha.Location = new System.Drawing.Point(64, 33);
+            this.tbarAlpha.Margin = new System.Windows.Forms.Padding(0);
+            this.tbarAlpha.Name = "tbarAlpha";
+            this.tbarAlpha.Size = new System.Drawing.Size(91, 33);
+            this.tbarAlpha.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(0, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 34);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Text : ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblColorInfo
+            // 
+            this.lblColorInfo.AutoSize = true;
+            this.lblColorInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblColorInfo.Location = new System.Drawing.Point(64, 66);
+            this.lblColorInfo.Margin = new System.Windows.Forms.Padding(0);
+            this.lblColorInfo.Name = "lblColorInfo";
+            this.lblColorInfo.Size = new System.Drawing.Size(212, 34);
+            this.lblColorInfo.TabIndex = 4;
+            this.lblColorInfo.Text = "Color [A=0, R=0, G=0, B=0]";
+            this.lblColorInfo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pColor
+            // 
+            this.pColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pColor.Location = new System.Drawing.Point(71, 5);
+            this.pColor.Margin = new System.Windows.Forms.Padding(7, 5, 5, 5);
+            this.pColor.Name = "pColor";
+            this.pColor.Size = new System.Drawing.Size(74, 23);
+            this.pColor.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(141, 144);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(222, 144);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // lboxColor
+            // 
+            this.lboxColor.FormattingEnabled = true;
+            this.lboxColor.ItemHeight = 15;
+            this.lboxColor.Location = new System.Drawing.Point(12, 173);
+            this.lboxColor.Name = "lboxColor";
+            this.lboxColor.Size = new System.Drawing.Size(285, 154);
+            this.lboxColor.TabIndex = 3;
+            // 
+            // pBackground
+            // 
+            this.pBackground.Controls.Add(this.panel6);
+            this.pBackground.Controls.Add(this.panel5);
+            this.pBackground.Controls.Add(this.panel4);
+            this.pBackground.Controls.Add(this.panel2);
+            this.pBackground.Controls.Add(this.panel3);
+            this.pBackground.Controls.Add(this.panel1);
+            this.pBackground.Location = new System.Drawing.Point(303, 21);
+            this.pBackground.Name = "pBackground";
+            this.pBackground.Size = new System.Drawing.Size(333, 306);
+            this.pBackground.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(102, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(65, 132);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(114, 146);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(127, 96);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(154, 59);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(123, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Location = new System.Drawing.Point(13, 166);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(123, 53);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Transparent;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Location = new System.Drawing.Point(36, 103);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(72, 76);
+            this.panel5.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Transparent;
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Location = new System.Drawing.Point(247, 179);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(50, 78);
+            this.panel6.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 339);
+            this.Controls.Add(this.pBackground);
+            this.Controls.Add(this.lboxColor);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbarAlpha)).EndInit();
+            this.pBackground.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pColor;
+        private System.Windows.Forms.TrackBar tbarAlpha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblColorInfo;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox lboxColor;
+        private System.Windows.Forms.Panel pBackground;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
