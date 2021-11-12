04ba1e2db415b65151cf6de1163afc86c7ad0a66
Seo Yeseul <sok98@naver.com>
2021-11-12 11:15:29 +0900

    32_linq

---
+++ 32_linq/32_linq/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace _32_linq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgView = new System.Windows.Forms.DataGridView();
+            this.btnLevel = new System.Windows.Forms.Button();
+            this.btnName = new System.Windows.Forms.Button();
+            this.btnAttribute = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.cboxAttribute = new System.Windows.Forms.ComboBox();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.numMax = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgView
+            // 
+            this.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgView.Location = new System.Drawing.Point(12, 12);
+            this.dgView.Name = "dgView";
+            this.dgView.RowTemplate.Height = 27;
+            this.dgView.Size = new System.Drawing.Size(413, 653);
+            this.dgView.TabIndex = 0;
+            // 
+            // btnLevel
+            // 
+            this.btnLevel.Location = new System.Drawing.Point(436, 12);
+            this.btnLevel.Name = "btnLevel";
+            this.btnLevel.Size = new System.Drawing.Size(173, 32);
+            this.btnLevel.TabIndex = 1;
+            this.btnLevel.Text = "Level 순으로";
+            this.btnLevel.UseVisualStyleBackColor = true;
+            this.btnLevel.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnName
+            // 
+            this.btnName.Location = new System.Drawing.Point(436, 50);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(173, 32);
+            this.btnName.TabIndex = 2;
+            this.btnName.Text = "Name 순으로";
+            this.btnName.UseVisualStyleBackColor = true;
+            this.btnName.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnAttribute
+            // 
+            this.btnAttribute.Location = new System.Drawing.Point(436, 88);
+            this.btnAttribute.Name = "btnAttribute";
+            this.btnAttribute.Size = new System.Drawing.Size(173, 32);
+            this.btnAttribute.TabIndex = 3;
+            this.btnAttribute.Text = "속성 순으로";
+            this.btnAttribute.UseVisualStyleBackColor = true;
+            this.btnAttribute.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(436, 264);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(173, 32);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Filter";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(436, 302);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(173, 32);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Cancel";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // cboxAttribute
+            // 
+            this.cboxAttribute.FormattingEnabled = true;
+            this.cboxAttribute.Location = new System.Drawing.Point(436, 202);
+            this.cboxAttribute.Name = "cboxAttribute";
+            this.cboxAttribute.Size = new System.Drawing.Size(173, 23);
+            this.cboxAttribute.TabIndex = 6;
+            // 
+            // numMin
+            // 
+            this.numMin.Location = new System.Drawing.Point(436, 233);
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(71, 25);
+            this.numMin.TabIndex = 7;
+            this.numMin.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numMax
+            // 
+            this.numMax.Location = new System.Drawing.Point(537, 233);
+            this.numMax.Name = "numMax";
+            this.numMax.Size = new System.Drawing.Size(72, 25);
+            this.numMax.TabIndex = 8;
+            this.numMax.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(513, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "~";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 677);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numMax);
+            this.Controls.Add(this.numMin);
+            this.Controls.Add(this.cboxAttribute);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnAttribute);
+            this.Controls.Add(this.btnName);
+            this.Controls.Add(this.btnLevel);
+            this.Controls.Add(this.dgView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMax)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgView;
+        private System.Windows.Forms.Button btnLevel;
+        private System.Windows.Forms.Button btnName;
+        private System.Windows.Forms.Button btnAttribute;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox cboxAttribute;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.NumericUpDown numMax;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 32_linq/32_linq/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _32_linq
+{
+    public partial class Form1 : Form
+    {
+        const string sLEVEL = "LEVEL";
+        const string sNAME = "NAME";
+        const string sATTRIBUTE = "ATTRIBUTE";
+
+        DataTable dt;
+
+        enum EnumName
+        {
+            슬라임,
+            가고일,
+            골렘,
+            코볼트,
+            고블린,
+            고스트,
+            언데드,
+            노움,
+            드래곤,
+            웜,
+            서큐버스,
+            데빌,
+            만티코어,
+            바실리스트,
+        }
+
+        enum EnumAttribute
+        {
+            불,
+            물,
+            바람,
+            번개,
+            어둠,
+            빛,
+            땅,
+            나무,
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTableCreate();  // DataTable 생성
+            EnemyCreate();      // Data 정보 생성
+            CboxCreate();
+        }
+
+        private void CboxCreate()
+        {
+            foreach (EnumAttribute oAtt in Enum.GetValues(typeof(EnumAttribute)))
+            {
+                cboxAttribute.Items.Add(oAtt);
+            }
+            cboxAttribute.SelectedIndex = 0;
+        }
+
+        private void DataTableCreate()
+        {
+            dt = new DataTable("Enemy");
+            
+            //DataColumn 생성
+            DataColumn colLevel = new DataColumn(sLEVEL, typeof(int));
+            DataColumn colName = new DataColumn(sNAME, typeof(string));
+            DataColumn colAttribute = new DataColumn(sATTRIBUTE, typeof(string));
+
+            dt.Columns.Add(colLevel);
+            dt.Columns.Add(colName);
+            dt.Columns.Add(colAttribute);
+        }
+        private void EnemyCreate()
+        {
+            Random rd = new Random();
+
+            foreach (Enum oName in Enum.GetValues(typeof(EnumName)))
+            {
+                DataRow dr = dt.NewRow();
+
+                dr[sLEVEL] = rd.Next(1, 11);    // 1부터 10까지 랜덤 숫자
+                dr[sNAME] = oName.ToString();
+
+                int iAttribute = Enum.GetValues(typeof(EnumAttribute)).Length;
+                dr[sATTRIBUTE] = (EnumAttribute)(rd.Next(iAttribute));
+
+                dt.Rows.Add(dr);
+            }
+
+            dgView.DataSource = dt;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            Button oBtn = sender as Button;
+            DataTable dtCopy = dgView.DataSource as DataTable;
+            IEnumerable<DataRow> vSortTable = null;
+
+            switch (oBtn.Name)
+            {
+                case "btnLevel":
+                    vSortTable = from oRow in dtCopy.AsEnumerable()
+                                 orderby oRow.Field<int>(sLEVEL)    // 정렬 기준
+                                 select oRow;
+                    break;
+                case "btnName":
+                    vSortTable = from oRow in dtCopy.AsEnumerable()
+                                 orderby oRow.Field<string>(sNAME)    // 정렬 기준
+                                 select oRow;
+                    break;
+                case "btnAttribute":
+                    vSortTable = from oRow in dtCopy.AsEnumerable()
+                                 orderby oRow.Field<string>(sATTRIBUTE)    // 정렬 기준
+                                 select oRow;
+                    break;
+            }
+            int i = vSortTable.Count();
+
+            dtCopy = vSortTable.CopyToDataTable();
+            dgView.DataSource = dtCopy;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataTable dtCopy = dgView.DataSource as DataTable;
+            IEnumerable<DataRow> vSortTable = from oRow in dtCopy.AsEnumerable()
+                                              where oRow.Field<string>(sATTRIBUTE) == cboxAttribute.Text &&
+                                              oRow.Field<int>(sLEVEL) >= numMin.Value && oRow.Field<int>(sLEVEL) <= numMax.Value
+                                              select oRow;
+            int i = vSortTable.Count();
+            if (vSortTable.Count() > 0)
+            {
+                dtCopy = vSortTable.CopyToDataTable();
+                dgView.DataSource = dtCopy;
+            }
+            else
+            {
+                MessageBox.Show("검색 조건에 맞는 Data가 없습니다.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dgView.DataSource = dt;
+        }
+    }
+}
